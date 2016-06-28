6699206638e051afb7de2db5d2fa9359586210c6
Mahmud13 <mahmud.bracu@gmail.com>
2016-06-29 03:13:18 +0600

    heading changed

--- public_html/resources/templates/header.php
+++ public_html/resources/templates/header.php
@@ -5,6 +5,7 @@
 		<title><?php echo $pagetitle; ?></title>
 		<link rel="stylesheet" type="text/css" href="css/style.css" >
 		<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 		<script src="js/script.js" type="text/javascript"></script>
 	</head>
 	<body>
