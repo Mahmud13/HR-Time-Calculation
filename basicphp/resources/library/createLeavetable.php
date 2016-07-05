c14191578e10ceb473dd7d91bf99593fec619b23
Mahmud Abdullah <mahmud.bracu@gmail.com>
2016-07-05 22:17:50 +0600

    desktop.ini removed

--- basicphp/resources/library/createLeavetable.php
+++ basicphp/resources/library/createLeavetable.php
@@ -6,6 +6,7 @@ $staffpin = html($_POST['staffpin']);
 $year = html($_POST['year']);
 $month = html($_POST['month']);
 
+//Get the name of the department
 $sql = "SELECT `name` FROM departments WHERE `id`=$departmentid";
 $result = mysqli_query($link, $sql);
 if(!$result){
@@ -14,6 +15,7 @@ if(!$result){
 $row = mysqli_fetch_array($result);
 $department = $row['name'];
 
+//Get name and Designation of the empolyee from database
 $sql = "SELECT `name`, `des` FROM RawNameTable WHERE `pin`=$staffpin";
 $result = mysqli_query($link, $sql);
 if(!$result){
@@ -22,12 +24,17 @@ if(!$result){
 $row = mysqli_fetch_array($result);
 $staffname = $row['name'];
 $designation = $row['des'];
-$monthid = date("m", strtotime("2015-$month-23"));
+
+//Get month number from the name of the month and the length of the month
+$monthid = date("m", strtotime("2015-$month-01"));
 $startdate = "$year-$monthid-01";
-$endday = date("t", strtotime($startdate));
-$enddate = "$year-$monthid-$endday";
+$length_of_month = date("t", strtotime($startdate));
+$enddate = "$year-$monthid-$length_of_month";
+
+
+//Get the data for everyday of the month
 $sql = 'SELECT `date` , `inTime` , `outTime` , `status`, `enjoyedleave`, `earnedleave`,
-	`medicalleave`,`casualleave`,`leavewithoutpay`'. ' FROM RawTimeTable' . ' WHERE `pin` = "'.$staffpin .'"' . ' AND `date` >= STR_TO_DATE( "'.$year.'-'.$monthid.'-01", "%Y-%m-%d" ) ' . ' AND `date` <= STR_TO_DATE( "'.$year.'-'.$monthid.'-'.$endday.'", "%Y-%m-%d" ) ';
+	`medicalleave`,`casualleave`,`leavewithoutpay`'. ' FROM RawTimeTable' . ' WHERE `pin` = "'.$staffpin .'"' . ' AND `date` >= STR_TO_DATE( "'.$year.'-'.$monthid.'-01", "%Y-%m-%d" ) ' . ' AND `date` <= STR_TO_DATE( "'.$year.'-'.$monthid.'-'.$length_of_month.'", "%Y-%m-%d" ) ';
 $result = mysqli_query($link, $sql);
 if(!$result){
 	exit();
@@ -48,6 +55,8 @@ while ($row = mysqli_fetch_array($result)){
 		$workhour[$today] = date("g:s", strtotime($outtime[$today])-strtotime($intime[$today]));
 	}
 }
+
+//Get the status of the day where it's a holiday or workday
 $sql = 'SELECT `flag`, `date` FROM calendar WHERE `date`>="'.$startdate.'" AND `date`<="'.$enddate.'"';
 $result = mysqli_query($link, $sql);
 if(!$result){
@@ -58,6 +67,8 @@ while ($row = mysqli_fetch_array($result)){
 	$flags[$today] = $row['flag'];
 
 }
+
+//Get the balance of the previous month if the month is not January
 if($monthid != 1){
 	$prevmonth = $monthid-1;
 	$sql =  'SELECT * FROM RawMonthTable WHERE `pin`="'.$staffpin.'" AND YEAR(`month`)="'.$year.'" AND MONTH(`month`)="'.$prevmonth.'"';
@@ -68,23 +79,20 @@ if($monthid != 1){
 		exit();
 	}
 	if($row = mysqli_fetch_array($result)){
-		$earn_leave_balance=$row['earnedLeave'];
+		$earned_leave_balance=$row['earnedLeave'];
 		$medical_leave_balance=$row['medicalLeave'];
 		$casual_leave_balance=$row['casualLeave'];
-		$lates = $row['lates'];
 		$halves = $row['halves'];
 		$absents = $row['absents'];
 	}
-	if(empty($earn_leave_balance)){
-		$earn_leave_balance= '0';
+	if(empty($earned_leave_balance)){
+		$earned_leave_balance= '0';
 	}
 	if(empty($medical_leave_balance)){
 		$medical_leave_balance= '0';
 	}
 	if(empty($casual_leave_balance)){
 		$casual_leave_balance= '0';
-		$leaves_without_pay = '0'; 
-		$lates ='0';
 	}
 	if(empty($halves)){
 		$halves = 0;
@@ -93,151 +101,194 @@ if($monthid != 1){
 		$absents = 0;
 	}
 }else{
-	$earn_leave_balance = 12;
+	//If the month is January get the earned leave balance from December of the previous year
+	$prevyear = --$year;
+	$sql = "SELECT `earnedleavebalance` FROM RawYearTable WHERE pin=$staffpin AND YEAR(`year`)=$prevyear";
+	$result = mysqli_query($link, $sql);
+	if(!$result){
+		$error = "unable to get year balance" . mysqli_error($link);
+		include 'error.html.php';
+		exit();
+	}
+	if($row=mysqli_fetch_array($result)){
+		$earned_leave_balance = $row['earnedleavebalance'];
+	}
+	if(empty($earned_leave_balance)){
+		$earned_leave_balance = 0;
+	}
 	$medical_leave_balance = 14;
 	$casual_leave_balance = 21;
-	$leaves_without_pay = 0;
-	$lates = 0;
 	$halves = 0;
 	$absents = 0;
 }
+$lates = 0;
+$leaves_without_pay = '0'; 
 
-for($daycounter = "1";$daycounter<=$endday;$daycounter++){
-	$days[] = $daycounter;
-	$dt = $daycounter<10 ? "$year-$monthid-0$daycounter" : "$year-$monthid-$daycounter";
-	$dayNames[$daycounter]=date("l",strtotime($dt));
-	if(empty($flags[$daycounter])){
-		if($dayNames[$daycounter]  == 'Friday' or $dayNames[$daycounter] == 'Saturday'){
-			$flags[$daycounter] = 'holiday';
+//Evaluate the parameters of each day of the month
+for($day = "1";$day<=$length_of_month;$day++){
+	$days[] = $day;
+	$date = $day<10 ? "$year-$monthid-0$day" : "$year-$monthid-$day";
+	$dayNames[$day]=date("l",strtotime($date));
+	//If flag of the day is not available from the pristine calendar, assign default flags
+	if(empty($flags[$day])){
+		if($dayNames[$day]  == 'Friday' or $dayNames[$day] == 'Saturday'){
+			$flags[$day] = 'holiday';
 		}else{
-			$flags[$daycounter] = 'workday';
+			$flags[$day] = 'workday';
 		}
 	}
-	if(!isset($intime[$daycounter])){
-		$intime[$daycounter] = "-";
+	//No inTime or outTime data are availabe for the day e.g. a holiday
+	if(!isset($intime[$day])){
+		$intime[$day] = "-";
 	}
-	if(!isset($outtime[$daycounter])){
-		$outtime[$daycounter] = "-";
+	if(!isset($outtime[$day])){
+		$outtime[$day] = "-";
 	}
-	if($intime[$daycounter]=="00:00:00"){
-		$intime[$daycounter] = "-";
+	//The null flag is selected as "-00:00:01"
+	if($intime[$day]=="-00:00:01"){
+		$intime[$day] = "-";
 	}
-	if($outtime[$daycounter]=="00:00:00"){
-		$outtime[$daycounter] = "-";
+	if($outtime[$day]=="-00:00:01"){
+		$outtime[$day] = "-";
 	}
-	if($intime[$daycounter]!="-" and $outtime[$daycounter] != "-"){
-		$workhour[$daycounter] = date("G \h i \m", strtotime($outtime[$daycounter])-strtotime($intime[$daycounter]));
+	//If both inTime and outTime is available, determine the workhour
+	if($intime[$day]!="-" and $outtime[$day] != "-"){
+		$workhour[$day] = date("G \h i \m", strtotime($outtime[$day])-strtotime($intime[$day]));
 	}else{		
-		$workhour[$daycounter] = "-";
+		$workhour[$day] = "-";
 	}
 
-	if(!isset($medicalleave[$daycounter])){
-		$medicalleave[$daycounter]='0';
+	if(!isset($medicalleave[$day])){
+		$medicalleave[$day]='0';
 	}
 
-	if(!isset($casualleave[$daycounter])){
-		$casualleave[$daycounter]='0';
+	if(!isset($casualleave[$day])){
+		$casualleave[$day]='0';
 	}
-	$leavewithoutpay[$daycounter]=0;
-	if($intime[$daycounter] == "-" and $flags[$daycounter]!='holiday'){
-		$st = 'absent';
-		$ste = 'absent';
-		$absents++;
-	}else if($outtime[$daycounter]=="-" and $flags[$daycounter]!='holiday'){
-		$st = 'nopunchout';
-		$intime_epoch = strtotime($intime[$daycounter]);
+	$leavewithoutpay[$day]='0';
+	if($intime[$day] == "-" and $flags[$day]!='holiday'){
+		$present = 'absent';
+	}else if($outtime[$day]=="-" and $flags[$day]!='holiday'){
+		$intime_epoch = strtotime($intime[$day]);
 		$entrytime_epoch = strtotime("09:15:00");
 		$latetime_epoch = strtotime("09:30:00");
 		if($intime_epoch>$latetime_epoch){
-			$ste = "half";
+			$present = "!out-half";
 			if($halves>=12){
-				$ste = "absent";
+				$present = "!out-half12";
 			}
 			$halves++;
 		}else if($intime_epoch>$entrytime_epoch){
-			$ste = "late";
+			$present = "!out-late";
 			$lates++;
 			if($lates>=3){
-				$ste = 'absent';
+				$present= '!out-late3';
 				$lates = 0;
 			}
 		}else{
-			$ste = "full";
+			$present = '!out-full';
 		}
 
-	}else if($intime[$daycounter] != "-" and $flags[$daycounter]=='holiday'){
-		$st = 'pholiday';
-		$ste = $st;
-	}else if($flags[$daycounter]=='holiday'){
-		$st = 'aholiday';
-		$ste = $st;
+	}else if($intime[$day] != "-" and $flags[$day]=='holiday'){
+		$present = 'pholiday';
+	}else if($flags[$day]=='holiday'){
+		$present = 'aholiday';
 	}else{
-		$intime_epoch = strtotime($intime[$daycounter]);
-		$outtime_epoch = strtotime($outtime[$daycounter]);
+		$intime_epoch = strtotime($intime[$day]);
+		$outtime_epoch = strtotime($outtime[$day]);
 		$entrytime_epoch = strtotime("09:15:00");
 		$latetime_epoch = strtotime("09:30:00");
 		$leavetime_epoch = strtotime("13:30:00");
 		if($intime_epoch>$latetime_epoch or $outtime_epoch<$leavetime_epoch){
-			$st = "half";
+			$present = "half";
 			if($halves>=12){
-				$ste = "absent";
+				$present = "half12";
 			}
 			$halves++;
 		}else if($intime_epoch>$entrytime_epoch){
-			$st = "late";
+			$present = 'late';	
 			$lates++;
-			if($lates>=3){
-				$ste = 'absent';
-				$lates = 0;
+			if($lates>3){
+				$present = 'late3';
+				$lates=0;
 			}
 		}else{
-			$st = "full";
-			$ste = $st;
+			$present = 'full';
 		}
 	}
-	$status[$daycounter]=$st;	
-	$enjoyedleave[$daycounter] = 0;
-	if($ste=='absent'){
-		if($medicalleave[$daycounter] and $medical_leave_balance){
+	$status[$day] = $present;
+	$enjoyedleave[$day] = 0;
+	if(in_array($present, array('absent', 'half12', 'late3'))){
+		if($medicalleave[$day]!=0 and $medical_leave_balance){
 			$medical_leave_balance--;
-		}else if($casualleave[$daycounter] and $casual_leave_balance){
+		}else if($casualleave[$day]!=0 and $casual_leave_balance){
 			$casual_leave_balance--;
-		}else  if($earn_leave_balance>=1){
-			$enjoyedleave[$daycounter] = 1;
-			$earn_leave_balance--;
-		}else  if($earn_leave_balance<1 and $earn_leave_balance>=0.5){
-			$earn_leave_balance-=0.5;
-			$enjoyedleave[$daycounter] = 0.5;
+		}else  if($earned_leave_balance>=1){
+			$enjoyedleave[$day] = 1;
+			$earned_leave_balance--;
+		}else  if($earned_leave_balance<1 and $earned_leave_balance>=0.5){
+			$earned_leave_balance-=0.5;
+			$enjoyedleave[$day] = 0.5;
 			$leaves_without_pay+=0.5;
-			$leavewithoutpay[$daycounter]=0.5;
-			$earnedleave[$daycounter] = -0.029;
-			$earn_leave_balance-=0.029;
-		}else if($earn_leave_balance<0.5){
-			$leavewithoutpay[$daycounter]=1;
-			$earnedleave[$daycounter] = -0.058;
-			$earn_leave_balance-=0.058;
+			$leavewithoutpay[$day]=0.5;
+			if ($present!='late3'){
+				$earnedleave[$day] = -0.029;
+				$earned_leave_balance-=0.029;
+			}
+		}else if($earned_leave_balance<0.5){
+			$leavewithoutpay[$day]=1;
+			if ($present != 'late3'){
+				$earnedleave[$day] = -0.058;
+				$earned_leave_balance-=0.058;
+			}
 			$leaves_without_pay++;
 		}
-		$earnedleave[$daycounter]+=0.058;
-		$earn_leave_balance+=0.058;
-	}else if ($ste=='half'){
-		if($medicalleave[$daycounter] and $medical_leave_balance>=0.5){
+		$earnedleave[$day]+=0.058;
+		$earned_leave_balance+=0.058;
+	}else if ($present=='half' or $present=='!out-half'){
+		if($medicalleave[$day]!=0 and $medical_leave_balance>=0.5){
 			$medical_leave_balance-=0.5;
-		}else if($casualleave[$daycounter] and $casual_leave_balance>=0.5){
+		}else if($casualleave[$day]!=0 and $casual_leave_balance>=0.5){
 			$casual_leave_balance-=0.5;
-		}else if($earn_leave_balance>=0.5){
-			$enjoyedleave[$daycounter] = 0.5;
-			$earn_leave_balance-=0.5;
-		}else if($earn_leave_balance<0.5){
-			$leavewithoutpay[$daycounter]=0.5;
+		}else if($earned_leave_balance>=0.5){
+			$enjoyedleave[$day] = 0.5;
+			$earned_leave_balance-=0.5;
+		}else{
+			$leavewithoutpay[$day]=0.5;
 			$leaves_without_pay+=0.5;
+			$earnedleave[$day] = -0.029;
+			$earned_leave_balance -= 0.029;
 		}
-		$earnedleave[$daycounter] = 0.029;
-		$earn_leave_balance += 0.029;
+		$earnedleave[$day] += 0.058;
+		$earned_leave_balance += 0.058;
 	}else{
-		$earnedleave[$daycounter] = 0.058;
-		$earn_leave_balance += 0.058;
+		$earnedleave[$day] = 0.058;
+		$earned_leave_balance += 0.058;
 	}
-	$balance[$daycounter]="$earn_leave_balance";
+	$balance[$day]="$earned_leave_balance";
+}
+$sql = 'SELECT `pin` FROM RawMonthTable WHERE `pin`="'.$staffpin.'" AND `month`="'.$year.'-'.$monthid.'-00"';
+$result = mysqli_query($link, $sql);
+if(!$result){
+	$error = "Cannot connect to month". mysqli_error($link);
+	include 'error.html.php';
+	exit();
+}
+$row = mysqli_fetch_array($result);
+if(empty($row)){
+		$sql = 'INSERT INTO RawMonthTable(pin, month, lates, halves, absents, casualLeave, medicalLeave, earnedLeave, leaveswithoutpay)
+				VALUES("'.$staffpin.'","'.$year.'-'.$monthid.'-00","'.$lates.'","'.$halves.'","'.$absents.'","'.$casual_leave_balance.'","'.$medical_leave_balance.'","'.$earned_leave_balance.'","'.$leaves_without_pay.'")';
+}else{
+		$sql = "UPDATE RawMonthTable
+				SET pin=$staffpin, month=\"$year-$monthid-00\",
+				lates=$lates, halves=$halves, absents = $absents,
+				casualLeave = $casual_leave_balance, medicalLeave= $medical_leave_balance,
+				earnedLeave = $earned_leave_balance, leaveswithoutpay=$leaves_without_pay";
+}
+$result = mysqli_query($link, $sql);
+if(!$result){
+		$error = mysqli_error($link);
+		include 'error.html.php';
+		exit();
 }
 ?>
